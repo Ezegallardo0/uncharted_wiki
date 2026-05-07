64487d5e45afc89db04541873cabc96e049511e5
Ezegallardo0 <plumiferogaming@gmail.com>
2026-05-07 18:55:55 -0300

    "cards de juegos agregadas"

--- src/components/C_uncharted1.jsx
+++ src/components/C_uncharted1.jsx
@@ -1,25 +1,19 @@
 import "../styles/card1.css";
+import { juegos } from '../components/juegos';
 
 const Card1 = () => {
   return (
-    <div className="uncharted-container">
-
-      <div className="info">
-        <h1>Uncharted: Drake's Fortune</h1>
-
-        <p>
-          El juego sigue a Nathan Drake, un cazatesoros que busca el
-          legendario tesoro de Francis Drake y la mítica ciudad de El Dorado.
-          Durante la aventura lo acompañan Elena Fisher y Victor Sullivan.
-        </p>
-
-        <h2>Ver más información →</h2>
+    <div className="main-card">
+      <h1>Uncharted Saga</h1>
+      <div className="cards-container">
+        {juegos.map((juego, index) =>(
+          <div key={index} className="card">
+            <h2>{juego.title}</h2>
+            <p>{juego.descripcion}</p>
+            <img src={juego.img} alt={juego.title} />
+          </div>
+        ))}
       </div>
-
-      <img
-        src="https://static.wikia.nocookie.net/unchartedpedia/images/b/bd/Uncharted_1_portada.jpeg/revision/latest?cb=20211229031812&path-prefix=es"
-        alt="Uncharted Drake's Fortune"
-      />
     </div>
   );
 };
---
+++ src/components/juegos.js
@@ -0,0 +1,27 @@
+export const juegos = [
+    {
+        title:"Uncharted: Drake's Fortune",
+        descripcion: "Nathan Drake descubre un diario de Sir Francis Drake que lo lleva a una isla en busca del tesoro de El Dorado, enfrentándose a mercenarios y descubriendo que la estatua dorada es mortal.",
+        img: "https://static.wikia.nocookie.net/unchartedpedia/images/b/bd/Uncharted_1_portada.jpeg/revision/latest?cb=20211229031812&path-prefix=es"
+    },
+    {
+        title:"Uncharted 2: Among Thieves",
+        descripcion: "Drake se une a Chloe y Flynn para robar una lámpara mongola, pero todo se transforma en una carrera por la ciudad mítica de Shambhala, con Lazarević como antagonista.",
+        img: "https://cdn2.steamgriddb.com/grid/3e5ede0abd3ec9a6fe52b049e82c2aba.png"
+    },
+    {
+        title:"Uncharted 3: Drake's Deception",
+        descripcion:"Nate y Sully investigan un anillo que los lleva a la ciudad perdida de Iram en el desierto, enfrentándose a una sociedad secreta liderada por Marlowe.",
+        img: "https://static.wikia.nocookie.net/unchartedpedia/images/a/ae/Uncharted_3_portada.jpeg/revision/latest/thumbnail/width/360/height/360?cb=20211229182839&path-prefix=es"
+    },
+    {
+        title:"Uncharted 4: A Thief's End",
+        descripcion:"Drake, ya retirado, vuelve con su hermano Sam en una aventura por Libertalia.",
+        img:"https://upload.wikimedia.org/wikipedia/en/1/1a/Uncharted_4_box_artwork.jpg"
+    },
+    {
+        title:"Uncharted: The Lost Legacy",
+        descripcion:"Chloe y Nadine protagonizan una misión en la India, buscando el colmillo de Ganesha para evitar un conflicto",
+        img:"https://image.api.playstation.com/vulcan/img/rnd/202010/2813/h9Xc7ZhCEGUcS05YvpEcNT0D.png"
+    }
+]
